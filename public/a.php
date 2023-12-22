32ef3b85235a79c1de3bcb513b54f4d74fa57f18
gowano2022 <59827733+gowano2022@users.noreply.github.com>
2023-12-22 05:22:27 +0200

    Update a.php

--- public/a.php
+++ public/a.php
@@ -1,20 +1,17 @@
 <?php
 // Get the submitted name from the form
 $name = $_POST['name'];
-// Get the IP address of the user
-//$ip = $_SERVER['REMOTE_ADDR'];
-
 // Get the IP address of the user from the X-Forwarded-For header
 $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-
-
 
 // Telegram bot API URL and bot token
 $bot_url = 'https://api.telegram.org/bot6941631904:AAHfNlGSRsCLs23y6QMtFoq_p9o8p1gEW9w';
 // Telegram channel ID
 $channel_id = '-1001923193748';
 // Message to be sent to the channel
-$message = "Attendance: >> $name << has checked in from IP address: $ip.";
+$message =   " ◤❶◥
+Name ►►► $name
+IP ►►► $ip ||" ; 
 
 // Send the message to the Telegram channel using the bot API
 $telegram_url = $bot_url . '/sendMessage?chat_id=' . $channel_id . '&text=' . urlencode($message);
